a0c52143d3242d0fc7a7f915656d885f054acbb8
Jenni <justlittleoldmejenn@gmail.com>
2022-03-18 20:48:06 -0700

    Made workout page

---
+++ TheGreatestAppEver/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using TheGreatestAppEver.Code;
+
+namespace TheGreatestAppEver.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoverViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                 _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            DiscoverViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+
+        }
+    }
+}
